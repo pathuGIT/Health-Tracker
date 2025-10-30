e13616a1ccb00e240e24ff66f9685849b5f794f2
pathuGIT <wannigelakshan@gmail.com>
2025-10-30 08:51:11 +0530

    Update Api.js

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://health-tracker-orionx-escsczh2gcedbzfy.eastasia-01.azurewebsites.net/api',
 });
 
 // Attach token to requests, but NOT for refresh endpoint
